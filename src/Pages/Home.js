db79b9311a0e7e8288e5d6528faf2955936207c2
Alexander Santos <alexsantosantana@live.com>
2020-05-29 14:33:58 -0400

    staging for the next branch

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,40 +1,19 @@
-import React, { useEffect, useContext, useState } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
-import { FirebaseContext } from '../Firebase';
 import styled from 'styled-components';
 const HomePage = () => {
-	const { user } = useSelector((state) => state);
-	const [state, setState] = useState({
-		tickets: [],
-		locations: 0,
-		terminals: 0,
-	});
-	const firebase = useContext(FirebaseContext);
-	useEffect(() => {
-		const ticketsListener = firebase.getMasterTicketsListener((tickets) => {
-			setState((s) => ({ ...s, tickets }));
-		});
+	const state = useSelector((state) => state);
 
-		firebase.db
-			.collection(`${firebase.master}/locations`)
-			.get()
-			.then((snap) => {
-				setState((s) => ({ ...s, locations: snap.size }));
-			});
-		return () => {
-			ticketsListener();
-		};
-	}, [firebase]);
 	return (
 		<HomeStyled>
-			<h2>{user?.currentMaster?.name}</h2>
+			<h2>{state?.master?.name}</h2>
 			<div className='tickets'>
 				<h4>Open tickets</h4>
-				<p>{state.tickets.length}</p>
+				<p>{state.tickets.ids.length}</p>
 			</div>
 			<div className='locations'>
 				<h4>Total Locations</h4>
-				<p>{state.locations.length}</p>
+				<p>{state.locations.ids.length}</p>
 			</div>
 		</HomeStyled>
 	);
